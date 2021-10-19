012e090066bf58f19b586eb1cca12248b9ee7a27
Aisha Naveed <aisha@ailogica.com>
2021-10-19 22:37:31 +0100

    added romanToNum function

--- src/NumericToRoman.js
+++ src/NumericToRoman.js
@@ -1,40 +1,70 @@
 //utility function for array of objects
 const numUtility = (value, roman) => {
-    return {
-      value: value,
-      roman: roman
-    };
+  return {
+    value: value,
+    roman: roman
   };
-  
-  //number to Roman conversion main function
-  let RomanConvertor = num => {
-    if (isNaN(num)) throw new Error("Number is required");
-    if (num === 0) throw new Error("Number should not be 0");
-    if (num > 3999) throw new Error("Number is greater than 3999");
-    const numeral = [
-      numUtility(1000, 'M'),
-      numUtility(500, 'D'),
-      numUtility(400, 'CD'),
-      numUtility(100, 'C'),
-      numUtility(50, 'L'),
-      numUtility(10, 'X'),
-      numUtility(9, 'IX'),
-      numUtility(5, 'V'),
-      numUtility(4, 'IV'),
-      numUtility(1, 'I')
-    ];
-    let result = '';
-  
-    //We will check the if the entered number is greater than 0 or less than 3999
-    //bcz roman didn't have 0 in there counting and numbers above 3999 should be represented 
-    //with special bar over them.
-    if(!isNaN(num) && 3999 > num > 0){
-      numeral.forEach(item => {
-        for(; num >= item.value; num -= item.value)
-          result += item.roman;
-      });    
-    }
-    return result;
+};
+
+//number to Roman conversion function
+let numToRoman = num => {
+  if (isNaN(num)) throw new Error("Number is required");
+  if (num === 0) throw new Error("Number should not be 0");
+  if (num > 3999) throw new Error("Number is greater than 3999");
+
+  //array of object is deined to store the basic roman numerals against numbers 
+  const numeral = [
+    numUtility(1000, 'M'),
+    numUtility(500, 'D'),
+    numUtility(400, 'CD'),
+    numUtility(100, 'C'),
+    numUtility(50, 'L'),
+    numUtility(10, 'X'),
+    numUtility(9, 'IX'),
+    numUtility(5, 'V'),
+    numUtility(4, 'IV'),
+    numUtility(1, 'I')
+  ];
+  let result = '';
+
+  //We will check the if the entered number is greater than 0 or less than 3999
+  //bcz roman didn't have 0 in there counting and numbers above 3999 should be represented 
+  //with special bar over them.
+  if (!isNaN(num) && 3999 > num > 0)
+    numeral.forEach(item => {
+      for (; num >= item.value; num -= item.value)
+        result += item.roman;
+    });
+  return result;
+};
+
+//Roman to number conversion main function
+let romanIntoNum = str => {
+  if (!str || !isNaN(str)) throw new Error("Roman numeral is required");
+
+  //object is defined to check the value for basic roman numberals
+  const numeral = {
+    'M': 1000,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
   };
 
-module.exports = RomanConvertor;
+  let result = 0;
+  if (isNaN(str))
+    str.split('').forEach((item, index) => numeral[item] < numeral[str[index + 1]] ?
+      result -= numeral[item] : result += numeral[item]);
+  return result;
+};
+
+
+module.exports = {
+  numToRoman,
+  romanIntoNum
+};
--- src/NumericToRoman.test.js
+++ src/NumericToRoman.test.js
@@ -1,27 +1,45 @@
-const RomanConvertor = require("./NumericToRoman");
+const {
+    numToRoman,
+    romanIntoNum
+} = require("./NumericToRoman");
 
-test("will return error if the number is not passed", () => {
-    expect(() => RomanConvertor()).toThrow("Number is required");
-    expect(() => RomanConvertor("IV")).toThrow("Number is required");
+describe("numToRoman", () => {
+    test("will return error if the number is not passed", () => {
+        expect(() => numToRoman()).toThrow("Number is required");
+        expect(() => numToRoman("IV")).toThrow("Number is required");
+    });
+
+    test("will return error if the number is 0", () => {
+        expect(() => numToRoman(0)).toThrow("Number should not be 0");
+    });
+
+    test("will return error if the number is greater than 3999", () => {
+        expect(() => numToRoman(10870)).toThrow("Number is greater than 3999");
+    });
+
+    test("return appropriate roman number", () => {
+        expect(numToRoman(1)).toBe("I");
+        expect(numToRoman(3)).toBe("III");
+        expect(numToRoman(4)).toBe("IV");
+        expect(numToRoman(8)).toBe("VIII");
+        expect(numToRoman(10)).toBe("X");
+        expect(numToRoman(11)).toBe("XI");
+        expect(numToRoman(19)).toBe("XIX");
+        expect(numToRoman(59)).toBe("LIX");
+        expect(numToRoman(1087)).toBe("MLXXXVII");
+    });
 });
 
-test("will return error if the number is 0", () => {
-    expect(() => RomanConvertor(0)).toThrow("Number should not be 0");
-});
+describe("romanIntoNum", () => {
+    test("will return error if the roman numeral is not passed", () => {
+        expect(() => romanIntoNum()).toThrow("Roman numeral is required");
+        expect(() => romanIntoNum(0)).toThrow("Roman numeral is required");
+    });
 
-test("will return error if the number is greater than 3999", () => {
-    expect(() => RomanConvertor(10870)).toThrow("Number is greater than 3999");
+    test("return appropriate numeric for given roman number", () => {
+        expect(romanIntoNum('XIX')).toBe(19);
+        expect(romanIntoNum('CL')).toBe(150);
+        expect(romanIntoNum('MCLX')).toBe(1160);
+        expect(romanIntoNum('CCCLXXXVII')).toBe(387);
+    });
 });
-
-test("return appropriate roman number", () => {
-    expect(RomanConvertor(1)).toBe("I");
-    expect(RomanConvertor(3)).toBe("III");
-    expect(RomanConvertor(4)).toBe("IV");
-    expect(RomanConvertor(8)).toBe("VIII");
-    expect(RomanConvertor(10)).toBe("X");
-    expect(RomanConvertor(11)).toBe("XI");
-    expect(RomanConvertor(19)).toBe("XIX");
-    expect(RomanConvertor(59)).toBe("LIX");
-    expect(RomanConvertor(1087)).toBe("MLXXXVII");
-});
-
